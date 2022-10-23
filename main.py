d1eda862b8f8306e9da531c84bcc24644528906e
Makiwine <79422585+Makiwine@users.noreply.github.com>
2022-10-23 15:01:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import streamlit as st
+import plotly_express as px
 import pandas as pd
-import numpy as np
-import plotly.express as px
 import altair as alt
-import pickle  #to load a saved model
-import base64  #to open .gif files in streamlit app
+
 @st.cache(suppress_st_warning=True)
 def get_fvalue(val):
     feature_dict = {"No":1,"Yes":2}
@@ -16,11 +14,18 @@ def get_value(val,my_dict):
     for key,value in my_dict.items():
         if val == key:
             return value
-        
+
+st.title('COVID-19 DATASET DISPLAY :')
+st.image('covid19.jpg')
+st.markdown('Dataset :')
+df = pd.read_csv('day_wise.csv')
+df.drop(['Date'], axis=1, inplace=True)
+st.write(df.head(20))
+
+
 global numeric_columns
 global non_numeric_columns
 try:
-    st.write(df)
     numeric_columns = list(df.select_dtypes(['float', 'int']).columns)
     non_numeric_columns = list(df.select_dtypes(['object']).columns)
     non_numeric_columns.append(None)
@@ -28,46 +33,47 @@ try:
 except Exception as e:
     print(e)
 
-def load_data():
-    df=pd.read_csv("day_wise.csv")
-    
-    return df
-
-app_mode = st.sidebar.selectbox('Select Page',['Home']) #two pages
-st.title('COVID-19 DATASET DISPLAY :')
-st.image('covid19.jpg')
-data = load_data()
- # st.write(data)
-df= data.drop(["Date"], axis=1,inplace =True)
-st.markdown("dataset:")
-st.write(data.head(20))
-
-st.sidebar.subheader("Histogram Settings")
-st.markdown('Histogram')
-x = st.sidebar.selectbox('Select target column:', options=numeric_columns)
-bin_size = st.sidebar.slider("Number of Bins", min_value=10,max_value=100, value=40)
-plot = px.histogram(x=x, data_frame=data)
-st.plotly_chart(plot)
-    
-st.markdown('Scatterplot')
+st.title('Scatterplot')
 x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
 y_values = st.sidebar.selectbox('Y axis', options=numeric_columns)
-plot=alt.Chart(data).mark_circle().encode(x=x_values, y=y_values).interactive()
+plot=alt.Chart(df).mark_circle().encode(x=x_values, y=y_values).interactive()
 # display the char
-st.altair_chart(plot,use_container_width=True)
+st.altair_chart(plot,use_container_width=True)   
+
+
+st.sidebar.subheader("Histogram Settings")
+st.title('Histogram')
+x = st.sidebar.selectbox('Select target column:', options=numeric_columns)
+bin_size = st.sidebar.slider("Number of Bins", min_value=10,
+                             max_value=100, value=40)
+plot = px.histogram(x=x, data_frame=df)
+st.plotly_chart(plot)
+
+
+app_mode = st.sidebar.selectbox('Select Page',['Home']) #two pages
+
 if app_mode=='Home':
-    choises=st.selectbox('Which graphes you want to plot', ['line_chart','area_chart','Boxplot'])
-    
-    if choises=='line_chart':
-       st.markdown('Lineplots')
-       st.line_chart(df,width=1100, height=400)
+    choises=st.selectbox('Which graphes you want to plot', ['','bar_chart', 'line_chart','area_chart','Boxplot'])
+    if choises=='bar_chart':
+        st.markdown('Covid desease dataset by country/region ')
+        st.title('Bar chart of confirmed cases and Active cases')
+        st.bar_chart(df[['Confirmed','Active']].head(20))
+        st.title('Bar chart of confirmed cases and Deaths cases')
+        st.bar_chart(df[['Confirmed','Deaths']].head(20))
+        st.title('Bar chart of Deaths cases and Recovered one')
+        st.bar_chart(df[['Deaths','Recovered']].head(20))
+    elif choises=='line_chart':
+        st.markdown('Covid desease dataset by country/region ')
+        st.markdown('Lineplots')
+        st.line_chart(df,width=1100, height=400)
 
     elif choises=='area_chart':
+        
+        st.markdown('Covid desease dataset by country/region ')
         st.write('This is a area_chart.')
         st.area_chart(df)
     elif choises=='Boxplot':
+        st.markdown('Covid desease dataset by country/region ')
         st.markdown('Boxplot')
         plot = px.box(df)
         st.plotly_chart(plot)
-        
- 
